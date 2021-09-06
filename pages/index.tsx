709aee0cf43fe044e539c77c71b9b472a1fe0be5
nullworkbench <nullworkbench@gmail.com>
2021-09-06 15:29:43 +0900

    入力欄を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.scss";
 import Icon from "@/components/Icon";
 import ItemsJson from "@/data/items.json";
+import React, { useState } from "react";
 
 // 防災グッズ
 type Item = {
@@ -11,6 +12,20 @@ type Item = {
   explanation: string;
   iconType: string;
 };
+
+// 人
+type Gender = "man" | "woman";
+class Person {
+  name?: string;
+  gender: Gender;
+  age: number;
+
+  constructor(name?: string, gender: Gender, age: number) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+}
 
 type Props = {
   items: Item[];
@@ -24,6 +39,16 @@ export async function getStaticProps() {
 }
 
 const Home: NextPage<Props> = ({ items }: Props) => {
+  // 入力項目
+  const inputEntries = [
+    { name: "名前", type: "text", placeholder: "名前を入力（省略可）" },
+    { name: "年齢", type: "number", placeholder: "年齢を入力" },
+  ];
+
+  // 世帯人数
+  const Taro = new Person("太郎", "man", 21);
+  const [persons, setPersons] = useState<Person[]>([Taro]);
+
   return (
     <div className="container mx-auto">
       <Head>
@@ -42,11 +67,66 @@ const Home: NextPage<Props> = ({ items }: Props) => {
         </section>
 
         <section className="mt-10">
-          <input
-            type="number"
-            placeholder="同居人数を入力"
-            className="block mx-auto border-solid border-2 border-gray-500 rounded-lg p-2"
-          />
+          {/* 世帯人数の数だけ繰り返す */}
+          {persons.map((person, idx) => {
+            const InputWrap = (props: {
+              name: string;
+              children: React.ReactNode;
+            }) => {
+              return (
+                <div className="flex justify-center">
+                  <label htmlFor={`inputArea${idx}`} className="p-2 mr-4">
+                    {props.name}
+                  </label>
+                  {props.children}
+                </div>
+              );
+            };
+
+            const inputStyle =
+              "block border-solid border-2 border-gray-500 rounded-lg p-2";
+            return (
+              <div key={idx}>
+                <InputWrap name="名前">
+                  <input
+                    type="text"
+                    id={`inputArea${idx}`}
+                    placeholder="名前を入力（省略可）"
+                    className={inputStyle}
+                  />
+                </InputWrap>
+                <InputWrap name="性別">
+                  <select
+                    type="text"
+                    id={`inputArea${idx}`}
+                    placeholder="年齢を入力"
+                    className={inputStyle}
+                  >
+                    <option value="man">男性</option>
+                    <option value="woman">女性</option>
+                  </select>
+                </InputWrap>
+                <InputWrap name="年齢">
+                  <input
+                    type="text"
+                    id={`inputArea${idx}`}
+                    placeholder="年齢を入力"
+                    className={inputStyle}
+                  />
+                </InputWrap>
+              </div>
+            );
+          })}
+
+          {/* 人を追加する */}
+          <div>
+            <button
+              className="text-white font-bold bg-blue-400 px-8 py-3 rounded-lg"
+              onClick={() => setPersons([...persons, new Person("", "man", 0)])}
+            >
+              家族を追加
+            </button>
+          </div>
         </section>
 
         <section>
