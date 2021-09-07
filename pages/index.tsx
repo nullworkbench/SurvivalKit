1e90cdfce8efc650e0a8580c710e5164888d91c6
nullworkbench <nullworkbench@gmail.com>
2021-09-07 11:41:26 +0900

    人物アイコンを設定＆スタイルを調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -123,7 +123,7 @@ const Home: NextPage<Props> = ({ items }: Props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="pt-20 min-h-screen text-center">
+      <main className="pt-20 px-8 min-h-screen text-center">
         <section>
           <h1 className="text-6xl font-bold">Survival Kit</h1>
           <p className="mt-3">最低限必要な防災グッズを計算するツール</p>
@@ -131,58 +131,64 @@ const Home: NextPage<Props> = ({ items }: Props) => {
 
         <section className="mt-10">
           <h3>世帯人数：{persons.length}</h3>
-          {/* 世帯人数の数だけ繰り返す */}
-          {persons.map((person, idx) => {
-            const InputWrap = (props: {
-              name: string;
-              children: React.ReactNode;
-            }) => {
+          <div className="flex flex-wrap justify-center">
+            {/* 世帯人数の数だけ繰り返す */}
+            {persons.map((person, idx) => {
+              const InputWrap = (props: {
+                name: string;
+                children: React.ReactNode;
+              }) => {
+                return (
+                  <div className="flex justify-center pb-4">
+                    <label htmlFor={`inputArea${idx}`} className="p-2 mr-4">
+                      {props.name}
+                    </label>
+                    {props.children}
+                  </div>
+                );
+              };
+
+              const inputStyle =
+                "block border-solid border-2 border-gray-500 rounded-lg p-2";
               return (
-                <div className="flex justify-center pb-4">
-                  <label htmlFor={`inputArea${idx}`} className="p-2 mr-4">
-                    {props.name}
-                  </label>
-                  {props.children}
-                </div>
+                <form key={idx} className="border-black border-2 mb-2 p-8">
+                  {/* アイコン */}
+                  <div>
+                    <UserCircleIcon className="fill-current text-gray-500" />
+                  </div>
+                  {/* 名前 */}
+                  <div>
+                    <input
+                      type="text"
+                      id={`${idx}_inputArea1`}
+                      placeholder="名前を入力（省略可）"
+                      className="bg-transparent focus:outline-none"
+                    />
+                    <span>さん</span>
+                  </div>
+
+                  <InputWrap name="性別">
+                    <select
+                      id={`inputArea${idx}`}
+                      placeholder="年齢を入力"
+                      className={inputStyle}
+                    >
+                      <option value="man">男性</option>
+                      <option value="woman">女性</option>
+                    </select>
+                  </InputWrap>
+                  <InputWrap name="年齢">
+                    <input
+                      type="text"
+                      id={`inputArea${idx}`}
+                      placeholder="年齢を入力"
+                      className={inputStyle}
+                    />
+                  </InputWrap>
+                </form>
               );
-            };
-
-            const inputStyle =
-              "block border-solid border-2 border-gray-500 rounded-lg p-2";
-            return (
-              <form key={idx} className="border-black border-2 mb-2 p-8">
-                {/* 名前 */}
-                <div>
-                  <input
-                    type="text"
-                    id={`${idx}_inputArea1`}
-                    placeholder="名前を入力（省略可）"
-                    className="bg-transparent focus:outline-none"
-                  />
-                  <span>さん</span>
-                </div>
-
-                <InputWrap name="性別">
-                  <select
-                    id={`inputArea${idx}`}
-                    placeholder="年齢を入力"
-                    className={inputStyle}
-                  >
-                    <option value="man">男性</option>
-                    <option value="woman">女性</option>
-                  </select>
-                </InputWrap>
-                <InputWrap name="年齢">
-                  <input
-                    type="text"
-                    id={`inputArea${idx}`}
-                    placeholder="年齢を入力"
-                    className={inputStyle}
-                  />
-                </InputWrap>
-              </form>
-            );
-          })}
+            })}
+          </div>
 
           {/* 計算する */}
           <div>
